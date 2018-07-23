458fbe87b9115255e9809cc2303d1ef1b4b33d84
Stephen <macbook@Macbooks-MacBook-Pro-3.local>
2018-07-23 13:44:19 -0700

    developing logic around automating the navigation bar -- need to deal with staleness of elements

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC 
 import time
 
@@ -18,13 +19,38 @@ def team_login(driver, user, password):
     login = driver.find_element_by_id("login_button")
     login.click()
 
+def view_navigation_menu(driver):
+    """ returns navigation menu element to be used
+    with other navigation functions """
+    menu = driver.find_element_by_css_selector(".nav-left")
+    return menu
+
+def view_tutorial(driver):
+    """ opens up and steps through tutorial """
+    nav = view_navigation_menu(driver)
+    tutorial = driver.find_element_by_css_selector(".fb-init-tutorial")
+    tutPages = 8
+
+    actions = ActionChains(driver)
+    actions.move_to_element(nav)
+    time.sleep(2)
+    actions.click(tutorial)
+    actions.perform()
+    while tutPages != 0:
+        time.sleep(2)
+        nextBtn = driver.find_element_by_css_selector(".cta--yellow")
+        actions.move_to_element(nextBtn)
+        time.sleep(2)
+        actions.click(nextBtn)
+        actions.perform()
+        tutPages -= 1
 
 driver = webdriver.Firefox()
 user = "team1"
 password = "password3"
 
 # go to homepage 
-driver.get("https://13.57.214.36/index.php")
+driver.get("https://54.193.2.111/index.php")
 assert "Facebook CTF" in driver.title
 
 # login
@@ -32,9 +58,15 @@ team_login(driver, user, password)
 
 WebDriverWait(driver, 10).until(EC.title_contains("Facebook CTF | Gameboard"))
 assert "Facebook CTF | Gameboard" in driver.title, "not loaded properly"
-try:
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "g.highlighted:nth-child(131)")))
 
+view_tutorial(driver)
+
+""" try:
+    view_tutorial(driver)
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Skip to play")))
 finally:
-    country = driver.find_element_by_css_selector("g.highlighted:nth-child(131)")
-    country.click()
+    print "finally"
+    time.sleep(15)
+    driver.close() """
+
